68709368a6a3820a879b86207173def566708d3e
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-08-10 14:56:17 -0700

    slow

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-puts("Size of a char: %lu byte(s)", sizeof(char));
-puts("Size of an int: %lu byte(s)", sizeof(int));
-puts("Size of a long int: %lu byte(s))", sizeof(long int));
-puts("Size of a long long int: %lu byte(s)", sizeof(long long int));
-puts("Size of a float: %lu byte(s)", sizeof(float));
+puts("Size of a char: %lu byte(s)\n", sizeof(char));
+puts("Size of an int: %lu byte(s)\n", sizeof(int));
+puts("Size of a long int: %lu byte(s)\n)", sizeof(long int));
+puts("Size of a long long int: %lu byte(s\n)", sizeof(long long int));
+puts("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 }
