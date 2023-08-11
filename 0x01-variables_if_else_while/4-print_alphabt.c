54ea783e14d88160853e5bec9178c0e29dd5dc43
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-08-11 15:00:17 -0700

    eq not allowed

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ * main - outputs lowercase exept e and q alphabets.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alp[25] = "abcdfghijklmnoprstuvwxyz";
+int i;
+for (i = 0; i < 25; i++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
