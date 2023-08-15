1d41f3dbddd4e8cab384bfb39db48edfafa9ac60
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-08-15 12:18:26 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - if c is lowercase .
+ * @number:parameter
+ * Return: number if greater or equal to 0
+ *   -number if not
+ */
+int _abs(int number);
+{
+	if (number >= 0)
+		return (number);
+	else
+		return (-number);
+}
