992ccbe799ca721c1e9a9d6e4c1f9435b2ef3868
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-08-15 04:28:36 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * this func
+ * print alphabet.
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+{
+	char harf
+	for (harf == 'a', harf == 'z', harf++)
+		_putchar(harf);
+	_putchar('\n');
+}
