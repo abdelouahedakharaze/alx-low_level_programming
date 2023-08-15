dd9fb23a3beb251cf3a2434287a8c2c5614333db
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-08-15 05:52:33 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - if c is lowercase .
+ * @c:parameter
+ * Return: 1 if alpha
+ *   0 if not
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
