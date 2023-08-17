50d4e6ca593ad146015e3e30235d66d372325a14
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-08-17 15:32:16 -0700

    dsd

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - calculate multiplication of
+ * @a: first number
+ * @b: second number
+ * Return: the product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
