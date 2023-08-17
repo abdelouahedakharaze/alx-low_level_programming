7ead48665e882cd5b75e06edc63459a2a9288fe6
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-08-17 15:46:21 -0700

    asas

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - print
+ * numbers from 0-9 + new line
+*/
+void print_numbers(void)
+{
+	int a;
+
+	for (a == '0'; a <= '9'; a++)
+		puchar(a);
+	putchar('\n');
+}
