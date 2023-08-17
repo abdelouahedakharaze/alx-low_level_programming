5a3f5c12e603c11047ff8659cbf9079328bebdaf
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-08-17 13:17:11 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - see if a char is upper.
+ *Return: 1 if upper 0 if not
+ *@c: the char we are talking about.
+*/
+int _isupper(int c)
+{
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+
+}
