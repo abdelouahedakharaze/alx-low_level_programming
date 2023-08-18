ca1dbb61282442607c2d008d5a8568abff8d2e39
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-08-18 12:59:49 -0700

    sdsdd

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line - print a line
+ * the lenght of the line is
+ * @n: this value
+ */
+
+void print_line(int n)
+{
+	int counter;
+
+	if (n < 0)
+	{
+		for (counter = 0; counter <= n; counter++)
+		{
+			_putchar('_');
+		}
+	_putchar('\n');
+	}
+}
