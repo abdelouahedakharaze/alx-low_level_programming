6dc637c3372dea019e892a151f45b69036301d15
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-08-21 16:59:46 -0700

    opw

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,7 +9,7 @@ void print_rev(char *s)
 	int length = 0, stindex;
 
 	while (s[lenght]! = '\0')
-		length++;
+		lenght++;
 
 	for (stindex = length - 1; stindex >= 0; stindex--)
 		_putchar(s[stindex]);
