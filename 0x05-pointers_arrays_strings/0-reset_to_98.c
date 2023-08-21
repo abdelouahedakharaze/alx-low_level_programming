4bcffa0275a1549436b42c9241b6739c67685a49
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-08-21 13:43:43 -0700

    s

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
-/*
-* reset_to_98 - assigne value 98 to .
-*@*n: the pointer to be assigned to.
-*/
+/**
+ * reset_to_98 - assigne value 98 to .
+ *@n: the number to be changed.
+ */
 
 void reset_to_98(int *n)
 {
-    *n = 98;
+	*n = 98;
 }
