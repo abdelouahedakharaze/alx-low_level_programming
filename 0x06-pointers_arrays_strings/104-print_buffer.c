333e3dc382ee82c4291888374be0c32443e46ca6
abdelouahedakharaze <112736714+abdelouahedakharaze@users.noreply.github.com>
2023-09-20 20:31:41 +0100

    s

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,57 +1,37 @@
-#include "main.h"
+#include<stdio.h>
 /**
- * print_line - prints a s bytes of a buffer
- * @c: buffer
- * @s: bytes of buffer
- * @l: line of buffer
- * Return: void
- */
+* print_buffer - prints a string 10 lines with details
+* @b: string to print
+* @size: buffer size
+* Return: void
+*/
 
-void print_line(char *c, int s, int l)
-{
-	int j, k;
-
-	for (j = 0; j <= 9; j++)
-	{
-		if (j <= s)
-			printf("%02x", c[l * 10 + j]);
-		else
-			printf("  ");
-		if (j % 2)
-			putchar(' ');
-	}
-	for (k = 0; k <= s; k++)
-	{
-		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-			putchar(c[l * 10 + k]);
-		else
-			putchar('.');
-	}
-}
-
-/**
- * print_buffer - prints a buffer
- * @b: buffer to print
- * @size: size of buffer
- * Return: void
- */
 void print_buffer(char *b, int size)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	if (size <= 0)
 	{
-		printf("%08x: ", i * 10);
-		if (i < size / 10)
+		printf("\n");
+		return;
+	}
+
+	for (i = 0; i < size; i += 10)
+	{
+		int c = 0;
+
+		printf("%.8x: ", i);
+		for (j = i; c < 10; j++, c++)
 		{
-			print_line(b, 9, i);
+			(i + c < size) ? printf("%02x", b[j]) : printf("  ");
+			(j % 2) ? printf(" ") : 0;
 		}
-		else
+		c = 0;
+		for (j = i; c < 10 && i + c < size; j++, c++)
 		{
-			print_line(b, size % 10 - 1, i);
+			printf("%c", b[j] >= 32 ? b[j] : '.');
 		}
 		putchar('\n');
 	}
-	if (size == 0)
-		putchar('\n');
+
 }
