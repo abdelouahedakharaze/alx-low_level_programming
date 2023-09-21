d886ccf3d8b46cfbfc476265347597f9cff586b3
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-09-21 20:05:39 +0100

    one

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - the linked list
+ * @h: head of the list
+ * Return: the number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	int sum = 0;
+	const list_t *my_node = h;
+
+	if (!h)
+		return (0);
+
+	while (my_node)
+	{
+		sum++;
+		my_node->str ?
+		printf("[%u] %s\n", my_node->len, my_node->str) :
+		printf("[0] (nil)\n");
+		my_node = my_node->next;
+	}
+	return (sum);
+}
