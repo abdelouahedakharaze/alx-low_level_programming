26a7510f0afa2f5057d140a01f0eab028813b9c0
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-09-26 19:32:25 +0100

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,43 +2,42 @@
 
 /**
  * insert_nodeint_at_index - inserts a new node at a given position
- * @head: pointer to the head of the list
- * @idx: index of the list where the new node should be added
- * @n: data to be added to the new node
- * Return: address of the new node, or NULL if it failed
+ * @head:  head of the list
+ * @idx: index  where the new node should be added
+ * @n: data to be added
+ * Return: address of the new node, or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *tmp;
+	listint_t *new_node, *marhali;
 	unsigned int i;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL || head == NULL)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL || head == NULL)
 		return (NULL);
 
-	new->n = n;
+	new_node->n = n;
 
-	/* if idx == 0, insert at the beginning */
 	if (idx == 0)
 	{
-		new->next = *head;
-		*head = new;
-		return (new);
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
 
-	tmp = *head;
+	marhali = *head;
 	for (i = 0; i < idx - 1; i++)
 	{
-		if (tmp == NULL)
+		if (marhali == NULL)
 		{
-			free(new);
+			free(new_node);
 			return (NULL);
 		}
-		tmp = tmp->next;
+		marhali = marhali->next;
 	}
 
-	new->next = tmp->next;
-	tmp->next = new;
+	new_node->next = marhali->next;
+	marhali->next = new_node;
 
-	return (new);
+	return (new_node);
 }
