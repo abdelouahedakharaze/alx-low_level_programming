319aec0da631125114d4b2fcc1fc98fa3a5d928f
abdelouahedakharaze <abdelouahedpro1@gmail.com>
2023-10-02 18:33:26 +0100

    header

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+void file_copieur(const char *minni, const char *ilaika);
+#endif /* MAIN_H */
